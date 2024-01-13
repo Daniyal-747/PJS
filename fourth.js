cdcf4c92b0e178065819d865d9883df7a0bdfb50
Daniyal Mirza <danial.aptech96@gmail.com>
2024-01-13 20:33:48 +0500

    PJS LOOPS

---
+++ fourth.js
@@ -0,0 +1,67 @@
+// // // LOOPS
+
+// // for(let i=0; i<=10; i++){
+// //     console.log("PJS", i)
+// // }
+
+// // // STRING INTERPOLATION (Professionally this method is used)
+
+// // for(let i=0; i<=10; i++){
+// //     console.log(`The value of i is ${i}`);
+// // }
+
+// // Table
+
+
+// // for(let i=1; i<=20; i++){
+// //     console.log(`${5} x ${i} = ${5*i}`)
+// // }
+
+// let num = prompt("Enter Your Number")
+
+// for(let i=1; i<=10; i++){
+//     console.log(`${num} x ${i} = ${num*i}`)
+// }
+
+// // While Loop
+// // In while loop, increment condition is applied after console.
+
+// let myNum = 50;
+// while(myNum<=55){
+//     console.log("This is while loop", myNum)
+//     myNum++
+// }
+
+// // Do While Loop
+
+// let z = 10;
+// do{
+//     console.log("This is do while loop", z);
+//     z++;
+// } while (z<=15);
+
+// let gameNum = 30;
+// let userNum = prompt("Enter Your Number");
+
+// // (!) is a sign for NOT i.e. (userNum not equal to gameNum)
+// while(userNum !=gameNum){
+//     userNum = prompt("You guess the wrong number : Try Again");
+// }
+
+// swal("Congratulation! You wasted your 30 minutes")
+
+
+// For in Loop
+
+// let obj = {
+//     name : "Daniyal",
+//     age : 27,
+//     grade : "A+",
+//     city : "Karachi",
+//     number : 123
+// }
+
+// for(let n in obj){
+//     console.log(n, obj[n])
+// }
+
