bfcd4e6ecb112501d5d6abcca803b568e310070b
Daniyal Mirza <danial.aptech96@gmail.com>
2024-01-16 20:54:28 +0500

    MARKS SHEET

---
+++ marks.js
@@ -0,0 +1,33 @@
+// 5 Subjects. User will enter the marks
+
+let user = prompt("Enter Your Name")
+let MATHS = prompt("Enter Your Maths Marks");
+let ENGLISH = prompt("Enter Your English Marks");
+let SCIENCE = prompt("Enter Your Science Marks");
+let CHEMISTRY = prompt("Enter Your Chemistry Marks");
+let PHYSICS = prompt("Enter Your Physics Marks");
+
+console.log(user);
+console.log(MATHS);
+console.log(ENGLISH);
+console.log(SCIENCE);
+console.log(CHEMISTRY);
+console.log(PHYSICS);
+
+
+// let mathMarks = prompt("Enter Your Math Marks")
+
+// if(mathMarks>=90){
+//     console.log("Your Grade is A+")
+// }if(mathMarks>=80){
+//     console.log("Your Grade is A")
+// }if(mathMarks>=70){
+//     console.log("Your Grade is B");
+// }if(mathMarks>=60){
+//     console.log("Your Grade is C");
+// }if(mathMarks>=50){
+//     console.log("Your grade is D")
+// }else{
+//     console.log("you are fail")
+// }
+
